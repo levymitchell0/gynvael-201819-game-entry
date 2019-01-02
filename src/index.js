af56ca707b0c4c55e321d85a2e992a949438ef4a
Mitchell Levy <levymitchell0@gmail.com>
2019-01-02 11:08:10 -0800

    Created a skeleton JS file, with minor edits to index.html.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const pos0base = document.getElementById('pos0base').getContext('2d');
+const pos1base = document.getElementById('pos1base').getContext('2d');
+
+//TODO:
+//Have a way to model layers to make sure that user input is handled properly.
+//Actually design the game.
+class Layer {
+  constructor() {
+
+  }
+  push() {
+
+  }
+  pop() {
+
+  }
+}
