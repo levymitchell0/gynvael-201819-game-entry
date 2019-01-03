4383b749412873ba65c21f4228d2e09989c43d88
Mitchell Levy <levymitchell0@gmail.com>
2019-01-02 19:19:09 -0800

    Completed a simple, but useable, layer system, with a POC for overlap.

--- index.js
+++ index.js
@@ -8,14 +8,22 @@ console.log("hello");
 let stack = [[], []];
 class Layer {
   constructor(pos, init) {
-    this.elem = init();
+    this.elem = document.createElement("CANVAS");
+    this.elem.style.display = "none";
+    document.body.appendChild(this.elem);
+    this.elem.setAttribute('width', pos == 0 ? 1024 : 720);
+    this.elem.setAttribute('height', pos == 0 ? 768 : 480);
+    this.elem.setAttribute('class', 'pos' + pos);
+
+    init(this.elem);
+
     this.stackIndex = null;
     this.shown = false;
     this.pos = pos;
   }
   show() {
     this.elem.style.display = "block";
-    this.stackIndex = stack[this.pos].push(this) - 1;
+    //this.stackIndex = stack[this.pos].push(this) - 1;
     this.shown = true;
   }
   hide() {
@@ -24,28 +32,40 @@ class Layer {
   }
 }
 
-const pos0base = new Layer(0, () => {
-  let canvas = document.createElement("CANVAS");
-  document.body.appendChild(canvas);
-  canvas.setAttribute('width', 1024);
-  canvas.setAttribute('height', 768);
-  canvas.setAttribute('class', 'pos0');
-  return canvas;
+const pos0base = new Layer(0, (elem) => {
+  let ctx = elem.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+  ctx.stroke();
 });
 
 pos0base.show();
-console.log(stack[0][0].elem);
 
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
+const pos1base = new Layer(1, (elem) => {
+  let ctx = elem.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(95, 50, 40, 0, 2 * Math.PI);
+  ctx.stroke();
+});
 
-sleep(5000);
+pos1base.show();
 
-pos0base.hide();
-console.log(stack[0][0].elem);
+const pos0above = new Layer(0, (elem) => {
+  let ctx = elem.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(105, 50, 40, 0, 2 * Math.PI);
+  ctx.fillStyle = "red";
+  ctx.fill();
+});
+
+pos0above.show();
+
+const pos1above = new Layer(1, (elem) => {
+  let ctx = elem.getContext("2d");
+  ctx.beginPath();
+  ctx.arc(105, 50, 40, 0, 2 * Math.PI);
+  ctx.fillStyle = "red";
+  ctx.fill();
+});
+
+pos1above.show();
